c84b811d33c868f883dc909d9b9bcf9862d3cf10
Phung Luu Hoang Long <phungluuhoanglong@gmail.com>
2024-04-18 22:17:54 +0800

    next change

--- main.c
+++ main.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
+#include <conio.h>
+
+
+struct product {
+    char productName[50];
+    int quantity;
+    double price;
+};
+
+void add_product(struct product prod) {
+
+}
 
 int main() {
-    printf("hello world!");
+    struct product prod;
     return 0;
 }
