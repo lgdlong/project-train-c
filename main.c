6dd2ea42c6161bb469e22a5c748351a1b1579943
Phung Luu Hoang Long <phungluuhoanglong@gmail.com>
2024-04-20 20:53:31 +0700

    write menu

--- main.c
+++ main.c
@@ -30,55 +30,61 @@ struct cart {
 };
 
 void menu() {
-    ds_1();
+    int choice, choice1, choice2;
+
     while(1) {
-        int choose; lua_chon(&choose);
+        ds_1();
+        lua_chon(&choice);
 
-        if (choose == 1) { // Làm việc với sản phẩm
+        if (choice == 1) { // Làm việc với sản phẩm
             work_with_san_pham();
-            lua_chon(&choose);
+            lua_chon(&choice1);
 
-            printf("Nhap so luong san pham can them: ");
-            int productQuantity;
-            scanf("%d", &productQuantity);
+            if (choice1 == 1) { // Thêm sản phẩm
+                printf("Nhap so luong san pham can them: ");
+                int productQuantity;
+                scanf("%d", &productQuantity);
 
-            for (int i = 0; i < productQuantity; i++) {
-                if (choose == 1) {
+                for (int i = 0; i < productQuantity; i++) {
+                    printf("SAN PHAM %d\n", i+1);
+                    
                     printf("Nhap ten san pham\n");
                     printf("Nhap so luong san pham: \n");
                     printf("Nhap gia san pham: \n");
+                    printf("\n");
                     /*
                     Gán số thứ tự cho sản phẩm ngay tại thời điểm nhập vào một sản phẩm có kiểu product
                     */
                 }
+            }
 
-                else if (choose == 2) {
-                    printf("Lua chon so thu tu cua san pham can xoa: \n");
-                    /*
-                    Mỗi sản phẩm sẽ được gán một số thứ tự theo thời điểm nhập vào từ 1 -> productQuantity
-                    Khi xóa sản phẩm sẽ lựa chọn số thứ tự để xóa theo số thứ tự
-                    */
-                }
+            else if (choice1 == 2) {
+                printf("Lua chon so thu tu cua san pham can xoa: \n");
+                printf("\n");
+                /*
+                Mỗi sản phẩm sẽ được gán một số thứ tự theo thời điểm nhập vào từ 1 -> productQuantity
+                Khi xóa sản phẩm sẽ lựa chọn số thứ tự để xóa theo số thứ tự
+                */
+            }
 
-                else { // choose == 3 -> Sua san pham
-                    printf("Lua chon so thu tu cua san pham can sua: \n");
-                    printf("Sua ten san pham: \n");
-                    printf("Sua so luong san pham: \n");
-                    printf("Sua gia san pham: \n");
-
-                }
+            else { // choice1 == 3 -> Sua san pham
+                printf("Lua chon so thu tu cua san pham can sua: \n");
+                printf("Sua ten san pham: \n");
+                printf("Sua so luong san pham: \n");
+                printf("Sua gia san pham: \n");
+                printf("\n");
             }
             
         }
 
-        else if (choose == 2) {
+        else if (choice == 2) {
             work_with_don_mua();
         }
 
-        else if (choose == 3){
+        else if (choice == 3){
             work_with_gio_hang();
         }
-        else { // choose == 4
+        else { // choice == 4
             return;
         }
     }
--- menu.c
+++ menu.c
@@ -2,9 +2,10 @@
 #include <stdbool.h>
 
 
-void lua_chon(int *choose) {
+void lua_chon(int *choice) {
     printf("Moi nhap lua chon: ");
-    scanf("%d", choose);
+    scanf("%d", choice);
+    printf("\n");
 }
 
 void ds_1(){
@@ -13,20 +14,24 @@ void ds_1(){
     printf("2. Don mua\n");
     printf("3. Mua hang\n");
     printf("4. Thoat\n");
+    printf("\n");
 }
 
 void work_with_san_pham() {
     printf("1. Them san pham\n");
     printf("2. Xoa san pham\n");
     printf("3. Sua san pham\n");
+    printf("\n");
 }
 
 void work_with_don_mua() {
     printf("1. In danh sach tat ca san pham\n");
+    printf("\n");
     // printf("2. In danh sach tat ca san pham\n");
 }
 
 void work_with_gio_hang() {
     printf("1. In gio hang\n");
+    printf("\n");
     // printf("1. In danh sach tat ca san pham\n");
 }
