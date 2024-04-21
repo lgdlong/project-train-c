03ba61701575514e53f904715d576eb8f3f17eb1
Sua <pthoangtran0511@gmail.com>
2024-04-21 21:00:48 +0700

    fixbugandaddmore

--- main.c
+++ main.c
@@ -1,25 +1,66 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
-#include <conio.h>
 
-
-struct product {
+struct Product {
     char productName[50];
     int quantity;
     double price;
 };
 
-void add_product(struct product prod) {
+void addProduct(struct Product *products, int *product_count) {
+    printf("Nhap ten san pham: ");
+    getchar(); 
+    fgets(products[*product_count].productName, sizeof(products[*product_count].productName), stdin);
+    products[*product_count].productName[strcspn(products[*product_count].productName, "\n")] = '\0';
 
+    printf("Nhap so luong: ");
+    scanf("%d", &products[*product_count].quantity);
+
+    printf("Nhap gia san pham: ");
+    scanf("%lf", &products[*product_count].price);
+
+    (*product_count)++;
+}
+
+void displayProducts(struct Product *products, int product_count) {
+    printf("\nSan pham:\n");
+    int i;
+    for (i = 0; i < product_count; i++) {
+        printf("So: %d\n", i);
+        printf("Ten san pham: %s\n", products[i].productName);
+        printf("So luong: %d\n", products[i].quantity);
+        printf("Gia: %.2lf\n\n", products[i].price);
+    }
 }
 
 int main() {
-	int a = 5, b = 10;
-    printf("hello world!\n");
-    printf("chao moi nguoi minh ten la Long\n");
-    printf("Minh la K19\n");
-    int sum = a + b;
-    printf("Sum a + b = %d", sum);
+    struct Product products[50];
+    int product_count = 0;
+    int choice;
+
+    do {
+        printf("\n1. Them san pham\n");
+        printf("2. Hien thi danh sach san pham\n");
+        printf("3. Thoat\n");
+        printf("Nhap lua chon cua ban: ");
+        scanf("%d", &choice);
+
+        switch(choice) {
+            case 1:
+                addProduct(products, &product_count);
+                break;
+            case 2:
+                displayProducts(products, product_count);
+                break;
+            case 3:
+                printf("Ket thuc chuong trinh.\n");
+                break;
+            default:
+                printf("Lua chon khong hop le! Vui long chon lai.\n");
+        }
+    } while(choice != 3);
+
     return 0;
 }
+
--- product.c
+++ product.c
@@ -9,18 +9,28 @@ struct Product{
 	float price;
 };
 
-void addProduct(struct Product *products, int *product_count)
-{
-	printf("Nhap ten san pham: ");
-	getchar();
-	fgets(products[*product_count].name, sizeof(products[*product_count].name, stdin));
-	products[*product_count].name[strcspn(products[*product_count].name,"\n")]='\0';
-	
-	printf("Nhap so luong san pham: ");
-	scanf("%d", &products[product_count].quantity);
-	
-	printf("Nhap gia cua san pham: ");
-	scanf("%d", &products[product_count].price);
-	
-	(*product_count)++;
+void addProduct(struct Product *products, int *product_count) {
+    printf("Nhap ten san pham: ");
+    getchar(); 
+    fgets(products[*product_count].productName, sizeof(products[*product_count].productName), stdin);
+    products[*product_count].productName[strcspn(products[*product_count].productName, "\n")] = '\0'; // Lo?i b? ký t? newline
+
+    printf("Nhap so luong: ");
+    scanf("%d", &products[*product_count].quantity);
+
+    printf("Nhap gia san pham: ");
+    scanf("%lf", &products[*product_count].price);
+
+    (*product_count)++;
+}
+
+void displayProducts(struct Product *products, int product_count) {
+    printf("\nSan pham:\n");
+    int i;
+    for (i = 0; i < product_count; i++) {
+        printf("So: %d\n", i);
+        printf("Ten san pham: %s\n", products[i].productName);
+        printf("So luong: %d\n", products[i].quantity);
+        printf("Gia: %.2lf\n\n", products[i].price);
+    }
 }
