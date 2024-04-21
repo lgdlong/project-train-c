0674d8585cb063acc3923c0e0e2e1755a33ed0f1
Phung Luu Hoang Long <phungluuhoanglong@gmail.com>
2024-04-21 17:00:58 +0700

    link switch case co menu.c

--- main.c
+++ main.c
@@ -30,64 +30,33 @@ struct cart {
 };
 
 void menu() {
-    int choice, choice1, choice2;
+    int choice; // menu mẹ
+    int choice1; // menu con cấp 1
+    int choice2; // menu con cấp 2
 
-    while(1) {
+    do {
         ds_1();
         lua_chon(&choice);
 
-        if (choice == 1) { // Làm việc với sản phẩm
-            work_with_san_pham();
-            lua_chon(&choice1);
+        switch (choice) {
+            case 1: // Làm việc với sản phẩm
+                work_with_san_pham(choice1);
+            case 2: // Đơn mua: thông tin cá nhân, xuất đơn mua
+                work_with_don_mua();
+                break;
 
-            if (choice1 == 1) { // Thêm sản phẩm
-                printf("Nhap so luong san pham can them: ");
-                int productQuantity;
-                scanf("%d", &productQuantity);
+            case 3: // Giỏ hàng: thêm, xóa, sửa số lượng
+                work_with_gio_hang();
+                break;
 
-                for (int i = 0; i < productQuantity; i++) {
-                    printf("SAN PHAM %d\n", i+1);
-                    
-                    printf("Nhap ten san pham\n");
-                    printf("Nhap so luong san pham: \n");
-                    printf("Nhap gia san pham: \n");
-                    printf("\n");
-                    /*
-                    Gán số thứ tự cho sản phẩm ngay tại thời điểm nhập vào một sản phẩm có kiểu product
-                    */
-                }
-            }
-
-            else if (choice1 == 2) {
-                printf("Lua chon so thu tu cua san pham can xoa: \n");
-                printf("\n");
-                /*
-                Mỗi sản phẩm sẽ được gán một số thứ tự theo thời điểm nhập vào từ 1 -> productQuantity
-                Khi xóa sản phẩm sẽ lựa chọn số thứ tự để xóa theo số thứ tự
-                */
-            }
-
-            else { // choice1 == 3 -> Sua san pham
-                printf("Lua chon so thu tu cua san pham can sua: \n");
-                printf("Sua ten san pham: \n");
-                printf("Sua so luong san pham: \n");
-                printf("Sua gia san pham: \n");
-                printf("\n");
-            }
+            case 4:
+                break;
             
-        }
+            default:
+                printf("Invalid choice!\n");
 
-        else if (choice == 2) {
-            work_with_don_mua();
         }
-
-        else if (choice == 3){
-            work_with_gio_hang();
-        }
-        else { // choice == 4
-            return;
-        }
-    }
+    } while (1);
 }
 
 void add_product(struct product prod) {
--- menu.c
+++ menu.c
@@ -9,8 +9,8 @@ void lua_chon(int *choice) {
 }
 
 void ds_1(){
+    printf("--LUA CHON DOI TUONG LAM VIEC--\n");
     printf("1. San pham\n");
-    // printf("1. ");
     printf("2. Don mua\n");
     printf("3. Mua hang\n");
     printf("4. Thoat\n");
@@ -18,14 +18,51 @@ void ds_1(){
 }
 
 void work_with_san_pham() {
+    int choice1;
     printf("1. Them san pham\n");
     printf("2. Xoa san pham\n");
     printf("3. Sua san pham\n");
     printf("\n");
+    
+    lua_chon(&choice1);
+
+    switch (choice1) {
+        case 1:
+            printf("Nhap so luong san pham can them: ");
+            int productQuantity;
+            scanf("%d", &productQuantity);
+
+            for (int i = 0; i < productQuantity; i++) {
+                printf("SAN PHAM %d\n", i+1);
+                
+                printf("Nhap ten san pham\n");
+                printf("Nhap so luong san pham: \n");
+                printf("Nhap gia san pham: \n");
+                printf("\n");
+            }
+            /*
+            Gán số thứ tự cho sản phẩm ngay tại thời điểm nhập vào một sản phẩm có kiểu product
+            */
+        case 2:
+            printf("Lua chon so thu tu cua san pham can xoa: \n");
+            printf("\n");
+            /*
+            Mỗi sản phẩm sẽ được gán một số thứ tự theo thời điểm nhập vào từ 1 -> productQuantity
+            Khi xóa sản phẩm sẽ lựa chọn số thứ tự để xóa theo số thứ tự
+            */
+        case 3:
+            printf("Lua chon so thu tu cua san pham can sua: \n");
+            printf("Sua ten san pham: \n");
+            printf("Sua so luong san pham: \n");
+            printf("Sua gia san pham: \n");
+            printf("\n");
+        default:
+            return;
+    }
 }
 
 void work_with_don_mua() {
-    printf("1. In danh sach tat ca san pham\n");
+    printf("1. In danh sach cac san pham hien tai\n");
     printf("\n");
     // printf("2. In danh sach tat ca san pham\n");
 }
